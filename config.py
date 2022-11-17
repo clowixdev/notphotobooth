c58349bb2fcb258023bcb6b7305dc36800becdda
Nikita Mishenev <clowixdev@gmail.com>
2022-11-17 20:37:46 +1100

    adding footer and header: still in progress

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 secret_key = os.urandom(22).hex()
+csrf_key = os.urandom(22).hex()
 
 class main(object):
     DEBUG = True
     TESTING = False
 
     SECRET_KEY = str(secret_key)
+    CSRF_TOKEN = str(csrf_key)
 
     LANGUAGES = ['en', 'ru']
--- notphotobooth/views.py
+++ notphotobooth/views.py
@@ -3,9 +3,22 @@ from flask.helpers import flash
 
 from notphotobooth import app
 
+@app.route('/send-mail', methods=['POST', 'GET'])
+def sendMail():
+    if request.method == 'POST':
+        subject = request.form['subject']
+        return redirect(f'mailto:notphotobooth@gmail.com?subject={subject}')
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     title = 'Главная'
 
     return render_template('pages/index.html', title=title)
+
+@app.route('/portfolio', methods=['POST', 'GET'])
+def portfolio():
+
+    title = 'Портфолио'
+
+    return render_template('pages/portfolio.html', title=title)
